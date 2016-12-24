cefca8354b9fd1c8d58b5599f71c91086435366d
najibu <ns.najibu@gmail.com>
2016-12-24 17:27:31 +0000

    Fetching Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 
-Route::get('/', 'PagesController@home');
-Route::get('about', 'PagesController@about');
+Route::get('cards', 'CardsController@index');
+Route::get('cards/{card}', 'CardsController@show');
