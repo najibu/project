49e664adace58bf2cac7f9511c902c5551111c1f
najibu <ns.najibu@gmail.com>
2016-12-27 17:13:15 +0000

    Add auth users

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,5 +7,10 @@ Route::group(['middleware' => ['web']], function () {
   Route::post('cards/{card}/notes', 'NotesController@store');
   Route::get('notes/{note}/edit', 'NotesController@edit');
   Route::patch('notes/{note}', 'NotesController@update');
+
+  Route::auth();
+  Route::get('/home', 'HomeController@index');
 });
+
+
 
--- database/migrations/2016_12_27_170847_create_password_resets_table.php
+++ database/migrations/2016_12_27_170847_create_password_resets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('password_resets', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('email')->index();
+            $table->string('token')->index();
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('password_resets');
     }
 }
